7c703af90ea84560b2beef3699865c556b23aaad
Mostafizur-Pro <mostafizur0195@gmail.com>
2023-07-27 17:58:34 +0600

    github login successfully

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,8 +1,11 @@
 import { Layout, Menu, Button } from "antd";
 const { Header } = Layout;
 import Link from "next/link";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Navbar = () => {
+  const { data: session } = useSession();
+  console.log("object", session);
   return (
     <Header
       style={{
