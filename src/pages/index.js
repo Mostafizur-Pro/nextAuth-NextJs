5a956061319234653817101712b1c51e32536a95
Mostafizur-pro <mostafizur0195@gmail.com>
2023-07-28 08:39:40 +0600

    google login successfull

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,33 @@
 import Head from "next/head";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
 const HomePage = () => {
+  const { data: session } = useSession();
   return (
     <div>
       <Head>
         <title>Next Auth</title>
       </Head>
-      <h1 style={{ textAlign: "center", marginTop:"10%" }}>Welcome To Next Auth Home Page</h1>
+      <h1 style={{ textAlign: "center", marginTop: "10%" }}>
+        Welcome To Next Auth Home Page
+      </h1>
+
+      {session?.user?.email ? (
+        <>
+          <h2 style={{ textAlign: "center" }}>
+            Login User Name: {session?.user?.name}
+          </h2>
+          <h3 style={{ textAlign: "center" }}>
+            Login User Email: {session?.user?.email}
+          </h3>
+        </>
+      ) : (
+        <>
+          <h1 style={{ textAlign: "center" }}>
+            Login User Name: <Link href={"/login"}>Login</Link>
+          </h1>
+        </>
+      )}
     </div>
   );
 };
